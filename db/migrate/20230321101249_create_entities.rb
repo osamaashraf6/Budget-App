893e75a7f93cb86db523fc348dc7459741fffd43
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-23 00:32:07 +0200

    Add the migrate folder which contains the migrate files of the budget app in

---
+++ db/migrate/20230321101249_create_entities.rb
@@ -0,0 +1,11 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.integer :amount
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
