3a26b1436035041acb4f73ba0f36996728b6a775
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-24 22:38:33 +0200

    Add the controller for groups

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,51 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:public]
+
+  def index
+    @groups = Group.where(user_id: current_user.id)
+  end
+
+  def show
+    @entities = Entity.all.where(group_id: @group.id)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit; end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to group_url(@group), notice: 'Category was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.html { redirect_to group_url(@group), notice: 'group was successfully updated.' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+end
