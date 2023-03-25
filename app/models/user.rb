e8ddf02caffa1cf853a383652fcbf6a0237dfda9
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-25 15:58:07 +0200

    Try to fix the conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :groups, class_name: 'Group', dependent: :destroy, foreign_key: 'user_id'
   has_many :entities, class_name: 'Entitiy', dependent: :destroy, foreign_key: 'user_id'
