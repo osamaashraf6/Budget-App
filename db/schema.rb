ccd24d7e5b7e169e55ab3c02fa85f0c2c6e24116
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-25 21:02:05 +0200

    Fix the linter error

--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_322_204_150) do
+# rubocop:disable Metrics/BlockLength
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_204150) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'entities', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'amount'
-    t.bigint 'user_id'
-    t.bigint 'group_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['group_id'], name: 'index_entities_on_group_id'
-    t.index ['user_id'], name: 'index_entities_on_user_id'
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_entities_on_group_id"
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
-  create_table 'group_entities', force: :cascade do |t|
-    t.bigint 'group_id'
-    t.bigint 'entity_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['entity_id'], name: 'index_group_entities_on_entity_id'
-    t.index ['group_id'], name: 'index_group_entities_on_group_id'
+  create_table "group_entities", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "entity_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entity_id"], name: "index_group_entities_on_entity_id"
+    t.index ["group_id"], name: "index_group_entities_on_group_id"
   end
 
-  create_table 'groups', force: :cascade do |t|
-    t.string 'name'
-    t.string 'icon'
-    t.bigint 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_groups_on_user_id'
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.string 'role'
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "role"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'entities', 'groups'
-  add_foreign_key 'entities', 'users'
-  add_foreign_key 'group_entities', 'entities'
-  add_foreign_key 'group_entities', 'groups'
-  add_foreign_key 'groups', 'users'
+  add_foreign_key "entities", "groups"
+  add_foreign_key "entities", "users"
+  add_foreign_key "group_entities", "entities"
+  add_foreign_key "group_entities", "groups"
+  add_foreign_key "groups", "users"
 end
+# rubocop:enable Metrics/BlockLength
